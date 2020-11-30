a4dd1b09e7447f833f00a35685ffcd2fb1f93e92
Daniel <dlcomeaux@gmail.com>
2020-11-30 11:38:29 -0600

    New analysis on bike trip purpose

--- policy_analysis.r
+++ policy_analysis.r
@@ -1,5 +1,6 @@
-
+library(tidyverse)
 library(cmapplot)
+library(forcats)
 
 
 
@@ -15,7 +16,7 @@ setwd("C:/Users/Daniel/OneDrive - Chicago Metropolitan Agency for Planning/My Da
 
 # trips
 trips <- read_csv("place.csv") %>%
-  select(sampno, locno, perno, placeno, placeGroup, mode, distance, arrtime, deptime, travtime)
+  select(sampno, locno, perno, placeno, placeGroup, mode, distance, arrtime, deptime, travtime, tpurp)
 
 # person info
 ppl <- read_csv("person.csv") %>%
@@ -161,11 +162,162 @@ tt <- tt %>%
                                missing = "missing"))
 
 
+
+mdt <- mdt %>%
+  mutate(tpurp = factor(tpurp)) %>%
+  mutate(tpurp = recode(tpurp,
+                        "1"	= "Typical home activities",
+                        "2"	= "Worked at home (paid)",
+                        "3" = "Worked at fixed work location",
+                        "4"	= "Worked at non-fixed work location",
+                        "5"	= "Work related (off-site meeting)",
+                        "6"	= "Attended school or daycare / studied",
+                        "7"	= "Volunteered",
+                        "8" = "Shopped (non-routine like for appliances, cars, home furnishings)",
+                        "9"	= "Shopped (routine like grocery, clothing)",
+                        "10" = "Drive-thru errands (ATM, dry cleaning, pharmacy, etc.)",
+                        "11" = "Serviced a vehicle (purchased gas, regular maintenance)",
+                        "12" = "Health care visit for self",
+                        "13" = "Health care visit for someone else",
+                        "14" = "Visited a person staying at the hospital",
+                        "15" = "Non-shopping errands (banking, post office, government, etc.)",
+                        "16" = "Drive thru / take-out dining",
+                        "17" = "Ate / dined out",
+                        "18" = "Socialized with friends",
+                        "19" = "Socialized with relatives",
+                        "20" = "Attended a community event",
+                        "21" = "Attended a religious event",
+                        "22" = "Exercised outdoors",
+                        "23" = "Went to the gym",
+                        "24" = "Other recreation",
+                        "25" = "Attended a major special event",
+                        "26" = "Drop off / Pick up passenger(s) / child(ren)",
+                        "27" = "Accompanied someone else",
+                        "28" = "Changed travel mode / transferred",
+                        "97" = "Something else",
+                        "-7" = "Missing",
+                        "-8" = "Missing",
+                        "-9" = "Missing"
+  ))
+
+# condense into trip purpose categories
+mdt <- mdt %>%
+  mutate(tpurp.c = fct_collapse(tpurp,
+
+                                home = "Typical home activities",
+
+                                work = c("Worked at home (paid)",
+                                         "Worked at fixed work location",
+                                         "Worked at non-fixed work location",
+                                         "Work related (off-site meeting)"),
+
+                                school = "Attended school or daycare / studied",
+
+                                "shopping/errands" =
+                                  c("Shopped (non-routine like for appliances, cars, home furnishings)",
+                                    "Shopped (routine like grocery, clothing)",
+                                    "Drive-thru errands (ATM, dry cleaning, pharmacy, etc.)",
+                                    "Non-shopping errands (banking, post office, government, etc.)",
+                                    "Serviced a vehicle (purchased gas, regular maintenance)"),
+
+                                health = c("Health care visit for self",
+                                           "Health care visit for someone else",
+                                           "Visited a person staying at the hospital"),
+
+                                dining = c("Drive thru / take-out dining",
+                                           "Ate / dined out"),
+
+                                community = c("Socialized with friends",
+                                              "Socialized with relatives",
+                                              "Attended a community event",
+                                              "Attended a religious event"),
+
+                                "recreation/fitness" = c("Other recreation",
+                                               "Attended a major special event",
+                                               "Exercised outdoors",
+                                               "Went to the gym"),
+
+                                transport = c("Drop off / Pick up passenger(s) / child(ren)",
+                                              "Accompanied someone else"),
+
+                                transfer = "Changed travel mode / transferred",
+
+                                other = c("Something else",
+                                          "Volunteered"),
+
+                                missing = "Missing")
+  )
+
+
+tt <- tt %>%
+  mutate(tpurp = factor(TPURP)) %>%
+  mutate(tpurp = recode(tpurp,
+                        "1"	= "Working at home (for pay)",
+                        "2"	= "All other home activities",
+                        "3" = "Work/Job",
+                        "4"	= "All other activities at work",
+                        "5"	= "Attending class",
+                        "6"	= "All other activities at school",
+                        "7"	= "Change type of transportation/transfer",
+                        "8" = "Dropped off passenger from car",
+                        "9"	= "Picked up passenger",
+                        "10" = "Other - transportation",
+                        "11" = "Work/Business related",
+                        "12" = "Service private vehicle",
+                        "13" = "Routine shopping",
+                        "14" = "Shopping for major purpose",
+                        "15" = "Household errands",
+                        "16" = "Personal business",
+                        "17" = "Eat meal outside of home",
+                        "18" = "Health care",
+                        "19" = "Civic/religious activities",
+                        "20" = "Recreation/entertainment",
+                        "21" = "Visit friends/relatives",
+                        "24" = "Loop trip",
+                        "97" = "Other")) %>%
+  mutate(tpurp.c = fct_collapse(tpurp,
+                                home = "All other home activities",
+
+                                work = c("Working at home (for pay)",
+                                         "Work/Job",
+                                         "All other activities at work",
+                                         "Work/Business related"),
+
+                                school = c("Attending class",
+                                           "All other activities at school"),
+
+                                "shopping/errands" =
+                                  c("Routine shopping",
+                                    "Shopping for major purpose",
+                                    "Personal business",
+                                    "Service private vehicle",
+                                    "Household errands"),
+
+                                health = "Health care",
+
+                                dining = "Eat meal outside of home",
+
+                                community = c("Civic/religious activities",
+                                              "Visit friends/relatives"),
+
+                                "recreation/fitness" = "Recreation/entertainment",
+
+                                transport = c("Dropped off passenger from car",
+                                              "Picked up passenger",
+                                              "Other - transportation"),
+
+                                transfer = "Change type of transportation/transfer",
+
+                                other = c("Other",
+                                          "Loop trip")))
+
+
 #################################################
 #                                               #
 #            Analysis of driver vs. passenger   #
 #                                               #
 #################################################
+
 # Age bins
 breaks <- c(-1, 10, 17, 25, 30, 40, 50, 60, 70, 80, 90)
 age_labels <- c("5 to 9", "10 to 17", "18 to 24", "25 to 29", "30 to 39", "40 to 49",
@@ -233,3 +385,80 @@ rbind(d_vs_p_age_mdt, d_vs_p_age_tt) %>%
   pivot_wider(id_cols = c("survey"),names_from = "mode_c",values_from = c("mode_count")) %>%
   mutate(pax_share = passenger/driver)
 
+
+
+#################################################
+#                                               #
+#            Trip purpose of bike trips         #
+#                                               #
+#################################################
+
+
+## Create totals for trips by purpose category
+
+### Filter data
+all_mdt <- mdt %>%
+  filter(age < 90 & age >= 5 & mode_c != "missing")
+
+all_tt <- tt %>%
+  filter(AGE < 90 & AGE >= 5 & mode_c != "missing")
+
+### Calculate total number of trips
+all_purp_mdt <- all_mdt %>%
+  group_by(tpurp.c) %>%
+  summarize(tpurp.c_total = sum(wtperfin)) %>%
+  filter(tpurp.c != "missing")
+
+all_purp_tt <- all_tt %>%
+  group_by(tpurp.c) %>%
+  summarize(tpurp.c_total = sum(weight))
+
+
+# Create totals for trips by purpose category, specifically for bikes (reuse logic from above)
+bikes_mdt <- mdt %>%
+  filter(age < 90 & age>=5 & mode_c == "bike")
+
+bikes_tt <- tt %>%
+  filter(AGE < 90 & AGE>=5 & mode_c == "bike")
+
+bike_purp_mdt <- bikes_mdt %>%
+  group_by(tpurp.c) %>%
+  summarize(tpurp.c_count = sum(wtperfin)) %>%
+  inner_join(., all_purp_mdt, by = "tpurp.c") %>%
+  mutate(tpurp.c_pct = tpurp.c_count / tpurp.c_total, # Calculate share of bike trips out of all
+         survey = "mdt") %>% # add identifier
+  arrange(tpurp.c_pct) # sort by mode share
+
+bike_purp_tt <- bikes_tt %>%
+  group_by(tpurp.c) %>%
+  summarize(tpurp.c_count = sum(weight)) %>%
+  inner_join(., all_purp_tt, by = "tpurp.c") %>%
+  mutate(tpurp.c_pct = tpurp.c_count / tpurp.c_total, # Calculate share of bike trips out of all
+         survey = "tt") %>% # add identifier
+  arrange(tpurp.c_pct) # sort by mode share
+
+# Combine data from MDT and TT
+bike_purp <-
+  rbind(bike_purp_mdt,bike_purp_tt) %>%
+  select(-tpurp.c_total) # Remove total by purpose (now that bike share is calculated)
+
+# Graph mode share for the two surveys
+bike_mode_share <-
+  bike_purp %>%
+  filter(tpurp.c != "other") %>%
+  ggplot(aes(x = reorder(tpurp.c,desc(tpurp.c)), y = tpurp.c_pct, fill = survey)) +
+  geom_bar(stat = "identity",position = position_dodge2(width = .65, reverse = TRUE), width = .7) +
+  scale_y_continuous(labels = scales::label_percent()) +
+  coord_flip() +
+  theme_cmap(gridlines = "v")
+
+finalize_plot(bike_mode_share,
+              title = "Bicycle mode share by category of trips, 2008 vs. 2018.",
+              caption = "Source: CMAP analysis of MDT and TT data. Note that categories are not precise comparisons.")
+
+
+# Generate output table
+bike_purp %>%
+  mutate(tpurp.c_pct = paste0(round(tpurp.c_pct * 100,1),"%")) %>%
+  pivot_wider(names_from = "survey",
+              values_from = c("tpurp.c_count","tpurp.c_pct"))
