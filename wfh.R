25edba65de29938797a1f78f236a8425acf96134
Daniel Comeaux <DComeaux@cmap.illinois.gov>
2020-12-18 12:13:57 -0600

    Add home flag and fix joining.

--- wfh.R
+++ wfh.R
@@ -29,10 +29,23 @@ region <- read_csv("location.csv") %>%
   select(sampno, locno, out_region, county_fips, home)
 
 # home location flag
-home <- region %>%
-  filter(home == 1) %>%
+home_wip <- region %>%
+  filter(home == 1) %>% # identify home locations
   select(sampno,
          home_county = county_fips) %>%
+  distinct() # keep distinct home locations based on sample
+
+# Some households are coded as having home locations in multiple counties. Identify them.
+two_homes <- (home_wip %>%
+                group_by(sampno) %>%
+                summarize(n = n()) %>%
+                filter(n > 1))$sampno
+
+# Replace multi-county samples with a county_fips of 999
+home <- home_wip %>%
+  mutate(home_county = case_when(
+    sampno %in% two_homes ~ 999,
+    TRUE ~ home_county)) %>%
   distinct()
 
 # merge datasets
@@ -40,7 +53,7 @@ mdt <- trips %>%
   inner_join(ppl, by = c("sampno", "perno")) %>%
   inner_join(hh, by = "sampno") %>%
   inner_join(region, by = c("sampno", "locno")) %>%
-  left_join(home, by = c("sampno")) %>%
+  inner_join(home, by = c("sampno")) %>%
   filter(out_region==0 & distance<100)
 
 # add combined duration and distance for placeGroup trips
