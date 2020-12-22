1b472cc714b3f74fc965efb38c7bad8ec564f22e
Daniel Comeaux <DComeaux@cmap.illinois.gov>
2020-12-22 11:43:08 -0600

    Refinements
    Fixed descriptions and excluding zero-length trips

--- school_trips.R
+++ school_trips.R
@@ -17,7 +17,7 @@ setwd("C:/Users/dcomeaux/OneDrive - Chicago Metropolitan Agency for Planning/My 
 # trips
 trips <- read_csv("place.csv") %>%
   select(sampno, locno, perno, placeno, placeGroup, mode, distance, arrtime,
-         deptime, travtime, tpurp)
+         deptime, travtime, tpurp, hdist)
 
 # person info
 ppl <- read_csv("person.csv") %>%
@@ -202,7 +202,10 @@ all_school_mdt <- mdt %>%
     # Remove trips with modes either beginning or missing
     !(mode_c %in% c("missing","beginning")),
     # Keep only school trips
-    tpurp_c == "school") %>%
+    tpurp_c == "school",
+    # Keep only trips with nonzero distance
+    distance_pg > 0
+    ) %>%
   # Mutate to character to allow case_when modification
   mutate(mode_c_school = as.character(mode_c)) %>%
   # Separate out school buses from "other"
@@ -217,7 +220,8 @@ all_school_mdt <- mdt %>%
 all_school_tt <- tt %>%
   filter(AGE <= 18 & AGE >= 5,
          mode_c != "missing",
-         tpurp_c == "school") %>%
+         tpurp_c == "school",
+         DIST > 0) %>%
 mutate(mode_c_school = as.character(mode_c)) %>%
   mutate(mode_c_school = case_when(
     MODE == "school bus" ~ "school bus",
@@ -248,9 +252,9 @@ total_school_mode_c <-
 school_pct_plot <-
   total_school_mode_c %>%
   mutate(survey = recode_factor(survey,
-                                mdt = "My Daily Travel (2018)",
+                                mdt = "My Daily Travel (2019)",
                                 tt = "Travel Tracker (2008)"),
-         label = paste0(as.character(round(mode_c_pct*100,1)),"%")) %>%
+         label = paste0(format(round(mode_c_pct*100,1),nsmall = 1),"%")) %>%
   ggplot(aes(y = reorder(mode_c_school,desc(-mode_c_pct)), x = mode_c_pct, fill = survey)) +
   geom_col(position = position_dodge2(reverse = TRUE)) +
   geom_text(aes(label = label),position = position_dodge2(0.9,reverse = T), hjust = 0) +
@@ -259,21 +263,23 @@ school_pct_plot <-
   cmap_fill_discrete(palette = "friday")
 
 finalize_plot(school_pct_plot,
-              "Mode share of K-12 school trips, 2018 vs. 2018.",
+              "Mode share of K-12 school trips, 2018 vs. 2019.",
               "Note: Includes trips for travelers between 5 and 18 years old
               (inclusive). Overall school trips increased ~3% between the two
               surveys. Travel Tracker had two school-related trip categories
               (both included) while My Daily Travel had only one.
               <br><br>
-              Source: CMAP analysis of MDT and TT data.")
+              Source: CMAP analysis of MDT and TT data.",
+              filename = "school_foo_1",
+              mode = "png")
 
 # Chart of absolute numbers of school trips, MDT vs TT
 school_abs_plot <-
   total_school_mode_c %>%
   mutate(survey = recode_factor(survey,
-                                mdt = "My Daily Travel (2018)",
+                                mdt = "My Daily Travel (2019)",
                                 tt = "Travel Tracker (2008)"),
-         label = as.character(round(mode_c_total / 100))) %>%
+         label = round(mode_c_total / 100)) %>%
   ggplot(aes(y = reorder(mode_c_school,desc(-mode_c_total)), x = mode_c_total, fill = survey)) +
   geom_col(position = position_dodge2(reverse = TRUE)) +
   geom_text(aes(label = label),position = position_dodge2(0.9,reverse = T), hjust = 0) +
@@ -282,13 +288,15 @@ school_abs_plot <-
   cmap_fill_discrete(palette = "friday")
 
 finalize_plot(school_abs_plot,
-              "Total number of K-12 school trips by mode, 2018 vs. 2018 (in thousands).",
+              "Total number of K-12 school trips by mode, 2008 vs. 2019 (in thousands).",
               "Note: Includes trips for travelers between 5 and 18 years old
               (inclusive). Overall school trips increased ~3% between the two
               surveys. Travel Tracker had two school-related trip categories
               (both included) while My Daily Travel had only one.
               <br><br>
-              Source: CMAP analysis of MDT and TT data.")
+              Source: CMAP analysis of MDT and TT data.",
+              filename = "school_foo_2",
+              mode = "png")
 
 ### Look at it by income
 
@@ -337,9 +345,9 @@ total_school_inc_mode_c <-
 school_pct_inc_plot1 <-
   total_school_inc_mode_c %>%
   mutate(survey = recode_factor(survey,
-                                mdt = "My Daily Travel (2018)",
+                                mdt = "My Daily Travel (2019)",
                                 tt = "Travel Tracker (2008)"),
-         label = paste0(as.character(round(mode_c_pct*100,1)),"%")) %>%
+         label = paste0(format(round(mode_c_pct*100,1),nsmall = 1),"%")) %>%
   filter(income_c != "missing",
          mode_c_school == "walk"
          ) %>%
@@ -351,36 +359,40 @@ school_pct_inc_plot1 <-
   cmap_fill_discrete(palette = "friday")
 
 finalize_plot(school_pct_inc_plot1,
-              "Walk mode-share of K-12 school trips by household income category, 2018 vs. 2018.",
+              "Walk mode-share of K-12 school trips by household income category, 2008 vs. 2019.",
               "Note: Includes trips for travelers between 5 and 18 years old
               (inclusive).
               <br><br>
               Source: CMAP analysis of MDT and TT data.",
-              height = 5)
+              height = 5,
+              filename = "school_foo_3",
+              mode = "png")
 
 # Chart of mode share by income category
 school_pct_inc_plot2 <-
   total_school_inc_mode_c %>%
   mutate(survey = recode_factor(survey,
-                                mdt = "My Daily Travel (2018)",
+                                mdt = "My Daily Travel (2019)",
                                 tt = "Travel Tracker (2008)"),
-         label = paste0(as.character(round(mode_c_pct*100,1)),"%")) %>%
+         label = paste0(format(round(mode_c_pct*100,1),nsmall = 1),"%")) %>%
   filter(income_c != "missing") %>%
   ggplot(aes(y = reorder(mode_c_school,desc(-mode_c_pct)), x = mode_c_pct, fill = survey)) +
   geom_col(position = position_dodge2(reverse = TRUE)) +
   geom_text(aes(label = label),position = position_dodge2(0.9,reverse = T), hjust = 0) +
   theme_cmap(gridlines = "v") +
   facet_wrap(~income_c) +
-  scale_x_continuous(labels = scales::label_percent(),n.breaks = 4, limits = c(0,.55)) +
+  scale_x_continuous(labels = scales::label_percent(),n.breaks = 4, limits = c(0,.6)) +
   cmap_fill_discrete(palette = "friday")
 
 finalize_plot(school_pct_inc_plot2,
-              "Mode-share of K-12 school trips by household income category, 2018 vs. 2018.",
+              "Mode-share of K-12 school trips by household income category, 2008 vs. 2019.",
               "Note: Includes trips for travelers between 5 and 18 years old
               (inclusive).
               <br><br>
               Source: CMAP analysis of MDT and TT data.",
-              height = 8)
+              height = 8,
+              filename = "school_foo_4",
+              mode = "png")
 
 ## What about travel times by income for school trips
 
@@ -394,7 +406,7 @@ school_time_mdt <-
     income_c != "missing") %>%
   group_by(income_c) %>%
   summarize(travtime = weighted.mean(travtime_pg, w = wtperfin)) %>%
-  mutate(survey = "My Daily Travel (2018)")
+  mutate(survey = "My Daily Travel (2019)")
 
 # Repeat for TT
 school_time_tt <-
@@ -424,7 +436,9 @@ finalize_plot(school_time_plot,
               (inclusive). Trips with no travel time or lasting 150 minutes or
               more are excluded as outliers.
               <br><br>
-              Source: CMAP analysis of MDT and TT.")
+              Source: CMAP analysis of MDT and TT.",
+              filename = "school_foo_5",
+              mode = "png")
 
 
 ## What about trip distances by income for school trips
@@ -436,7 +450,7 @@ school_distance_mdt <-
          income_c != "missing") %>%
   group_by(income_c) %>%
   summarize(tripdist = weighted.mean(distance_pg, w = wtperfin)) %>%
-  mutate(survey = "My Daily Travel (2018)")
+  mutate(survey = "My Daily Travel (2019)")
 
 school_distance_tt <-
   all_school_tt %>%
@@ -464,7 +478,9 @@ finalize_plot(school_distance_plot,
               "Note: Includes trips for travelers between 5 and 18 years old
               (inclusive) that are greater than 0 miles.
               <br><br>
-              Source: CMAP analysis of MDT and TT.")
+              Source: CMAP analysis of MDT and TT.",
+              filename = "school_foo_6",
+              mode = "png")
 
 
 ## What about travel times by race for school trips
@@ -479,7 +495,7 @@ school_time_race_mdt <-
     race_eth != "missing") %>%
   group_by(race_eth) %>%
   summarize(travtime = weighted.mean(travtime_pg, w = wtperfin)) %>%
-  mutate(survey = "My Daily Travel (2018)")
+  mutate(survey = "My Daily Travel (2019)")
 
 # Chart of travel time to school by household income
 school_time_race_plot <-
@@ -495,7 +511,9 @@ finalize_plot(school_time_race_plot,
               (inclusive). Trips with no travel time or lasting 150 minutes or
               more are excluded as outliers.
               <br><br>
-              Source: CMAP analysis of MDT.")
+              Source: CMAP analysis of MDT.",
+              filename = "school_foo_7",
+              mode = "png")
 
 
 ## What about trip distances by income for school trips
@@ -507,7 +525,7 @@ school_distance_race_mdt <-
          race_eth != "missing") %>%
   group_by(race_eth) %>%
   summarize(tripdist = weighted.mean(distance_pg, w = wtperfin)) %>%
-  mutate(survey = "My Daily Travel (2018)")
+  mutate(survey = "My Daily Travel (2019)")
 
 # Chart of trip distances by household income
 school_distance_race_plot <-
@@ -522,7 +540,9 @@ finalize_plot(school_distance_race_plot,
               "Note: Includes trips for travelers between 5 and 18 years old
               (inclusive) that are greater than 0 miles.
               <br><br>
-              Source: CMAP analysis of MDT and TT.")
+              Source: CMAP analysis of MDT and TT.",
+              filename = "school_foo_8",
+              mode = "png")
 
 ##### Run regressions on income and race and ethnicity
 
