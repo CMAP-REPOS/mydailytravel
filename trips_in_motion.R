f90cd3e1445d0a250425c9dff2ed8f1a95568603
Daniel Comeaux <DComeaux@cmap.illinois.gov>
2021-01-13 18:02:48 -0600

    Fix weekend issue
    Include early Saturday morning trips and rename charts

--- trips_in_motion.R
+++ trips_in_motion.R
@@ -1,5 +1,3 @@
-
-
 library(ggplot2)
 library(lubridate)
 library(tidyverse)
@@ -22,11 +20,10 @@ source("data_cleaning.R")
 #                                               #
 #################################################
 
-# Convert to datetime object and add day of week
-tim_mdt <- mdt %>%
-  mutate(arrtime = ymd_hms(arrtime),
-  ) %>%
-  mutate(day_of_week = wday(arrtime))
+# Convert to datetime object
+tim_mdt <-
+  mdt %>%
+  mutate(arrtime = ymd_hms(arrtime))
 
 # Create helper values
 threshold <- as.numeric(ymd_hms("2020-01-01 03:00:00", tz = "America/Chicago"))
@@ -35,11 +32,13 @@ day_value <- 60*60*24
 
 # Process data
 tim_mdt_wip <-
-  tim_mdt %>%
-  # Remove weekends
-  filter(day_of_week != 1 & day_of_week != 7,
-         # Remove trips > 15 hours
-         travtime < 15 * 60 & travtime > 0) %>%
+  tim_mdt %>%                  # 125,103 records
+  filter(
+    # Remove trips > 15 hours
+    travtime < 15 * 60,        # 125,096 records
+    # Remove trips with no travel time
+    travtime_pg > 0            # 96,775 records
+    ) %>%
   # Make every trip on the same day (for analysis and graphing)
   mutate(trip_end = force_tz(ymd_hms(paste0("2020-01-01 ",
                                             substr(arrtime,12,19))),
@@ -78,10 +77,11 @@ trip_times <-
     to = as.POSIXct("2020-01-02 03:00:00", tz = "America/Chicago"),
     by = "5 min"))
 
-trip_times_mode_mdt <- trip_times %>%
+trip_times_mode_c_mdt <-
+  trip_times %>%
   # Add all possible modes to each time
   full_join(.,possible_modes, by = character()) %>%
-  # Calculate the number of trips that meet the criteria (in the interval and correct purpose)
+  # Calculate the number of trips that meet the criteria (in the interval and correct mode category)
   mutate(mode_count = mapply(function(x,y) sum(tim_mdt_wip$wtperfin[which(
     x %within% tim_mdt_wip$trip_interval &
       y == tim_mdt_wip$mode_c)]),
@@ -102,7 +102,8 @@ breaks <- seq.POSIXt(from = as.POSIXct("2020-01-01 03:00:00"),
                      by = "3 hours")
 
 # Graph output of trips in motion by mode
-chart2 <- trip_times_mode_mdt %>%
+trips_in_motion_p1 <-
+  trip_times_mode_c_mdt %>%
   filter(mode_c != "missing") %>%
   ggplot(aes(x = time_band,y = rolling_mode_count)) +
   geom_area(aes(fill = mode_c)) +
@@ -113,7 +114,7 @@ chart2 <- trip_times_mode_mdt %>%
   theme_cmap(gridlines = "hv",
              panel.grid.major.x = element_line(color = "light gray"))
 
-finalize_plot(chart2,
+finalize_plot(trips_in_motion_p1,
               "Trips in motion in the CMAP region by mode on weekdays.",
               "Source: CMAP analysis of My Daily Travel survey.")
 
@@ -121,7 +122,8 @@ finalize_plot(chart2,
 ### Mode and purpose
 
 
-trip_times_mode_and_purp.c_mdt <- trip_times %>%
+trip_times_mode_and_purp_c_mdt <-
+  trip_times %>%
   # Add all possible modes to each time
   full_join(.,possible_mode_tpurp_c, by = character()) %>%
   # Calculate the number of trips that meet the criteria (in the interval and correct mode)
@@ -139,7 +141,8 @@ trip_times_mode_and_purp.c_mdt <- trip_times %>%
 
 
 # Graph output of trips in motion by purpose for bike trips
-chart3 <- trip_times_mode_and_purp.c_mdt %>%
+trips_in_motion_p2 <-
+  trip_times_mode_and_purp_c_mdt %>%
   filter(mode == "bike share") %>%
   ggplot(aes(x = time_band,y = rolling_count)) +
   geom_area(aes(fill = tpurp_c)) +
@@ -150,14 +153,15 @@ chart3 <- trip_times_mode_and_purp.c_mdt %>%
   theme_cmap(gridlines = "hv",
              panel.grid.major.x = element_line(color = "light gray"))
 
-finalize_plot(chart3,
+finalize_plot(trips_in_motion_p2,
               "Bike share trips in motion by travel purpose.",
               "Note: Trips in motion are 55-minute rolling averages.
               <br><br>
               Source: CMAP analysis of My Daily Travel survey.")
 
 # Graph output of trips in motion by purpose for bike trips
-chart4 <- trip_times_mode_and_purp.c_mdt %>%
+trips_in_motion_p3 <-
+  trip_times_mode_and_purp_c_mdt %>%
   filter(mode == "rideshare" | mode == "bike share") %>%
   ggplot(aes(x = time_band,y = rolling_count)) +
   geom_area(aes(fill = tpurp_c)) +
@@ -169,7 +173,7 @@ chart4 <- trip_times_mode_and_purp.c_mdt %>%
   theme_cmap(gridlines = "hv",
              panel.grid.major.x = element_line(color = "light gray"))
 
-finalize_plot(chart4,
+finalize_plot(trips_in_motion_p3,
               "TNC and bike share trips in motion by travel purpose.",
               "Note: Trips in motion are 55-minute rolling averages.
               <br><br>
@@ -177,7 +181,8 @@ finalize_plot(chart4,
 
 
 # Graph output of trips in motion by purpose for bike trips
-chart5 <- trip_times_mode_and_purp.c_mdt %>%
+trips_in_motion_p4 <-
+  trip_times_mode_and_purp_c_mdt %>%
   filter(mode == "personal bike" | mode == "bike share") %>%
   ggplot(aes(x = time_band,y = rolling_count)) +
   geom_area(aes(fill = tpurp_c)) +
@@ -189,7 +194,7 @@ chart5 <- trip_times_mode_and_purp.c_mdt %>%
   theme_cmap(gridlines = "hv",
              panel.grid.major.x = element_line(color = "light gray"))
 
-finalize_plot(chart5,
+finalize_plot(trips_in_motion_p4,
               "Bicycling trips in motion by travel purpose.",
               "Note: Trips in motion are 55-minute rolling averages.
               <br><br>
@@ -197,7 +202,8 @@ finalize_plot(chart5,
 
 
 # Graph output of trips in motion by purpose for transit trips
-chart6 <- trip_times_mode_and_purp.c_mdt %>%
+trips_in_motion_p5 <-
+  trip_times_mode_and_purp_c_mdt %>%
   filter(mode %in% c("rail and bus", "bus", "train", "local transit", "transit")) %>%
   group_by(time_band,tpurp_c) %>%
   summarize(rolling_count = sum(rolling_count)) %>%
@@ -210,7 +216,7 @@ chart6 <- trip_times_mode_and_purp.c_mdt %>%
   theme_cmap(gridlines = "hv",
              panel.grid.major.x = element_line(color = "light gray"))
 
-finalize_plot(chart6,
+finalize_plot(trips_in_motion_p5,
               "Transit trips in motion by travel purpose.",
               "Note: Trips in motion are 55-minute rolling averages.
               <br><br>
@@ -219,7 +225,8 @@ finalize_plot(chart6,
 
 #### Group by trip chain buckets
 
-trip_times_bucket_mdt <- trip_times %>%
+trip_times_bucket_mdt <-
+  trip_times %>%
   full_join(.,possible_buckets, by = character()) %>%
   # Calculate the number of trips that meet the criteria (in the interval and correct bucket)
   mutate(bucket_count = mapply(function(x,y) sum(tim_mdt_wip$wtperfin[which(
@@ -234,7 +241,8 @@ trip_times_bucket_mdt <- trip_times %>%
   ungroup()
 
 
-chart7 <- trip_times_bucket_mdt %>%
+trips_in_motion_p6 <-
+  trip_times_bucket_mdt %>%
   ggplot(aes(x = time_band,y = rolling_bucket_count)) +
   geom_area(aes(fill = reorder(chain_bucket,desc(chain_bucket)))) +
   scale_x_datetime(labels = scales::date_format("%H:%M", tz = "America/Chicago"),
@@ -245,7 +253,7 @@ chart7 <- trip_times_bucket_mdt %>%
              panel.grid.major.x = element_line(color = "light gray"),
              legend.max.columns = 3)
 
-finalize_plot(chart7,
+finalize_plot(trips_in_motion_p6,
               "Trips in motion in the CMAP region by trip chain type on weekdays.",
               "Note: Trips in motion are 25-minute rolling averages.
               <br><br>
@@ -260,7 +268,8 @@ tim_mdt_drivethru <-
   # Filter to just the purpose in question
   filter(tpurp == "Drive thru / take-out dining")
 
-trip_times_drivethru_and_mode_mdt <- trip_times %>%
+trip_times_drivethru_and_mode_mdt <-
+  trip_times %>%
   # Add all possible modes to each time
   full_join(.,possible_modes, by = character()) %>%
   # Calculate the number of trips that meet the criteria (in the interval and correct mode)
@@ -277,7 +286,8 @@ trip_times_drivethru_and_mode_mdt <- trip_times %>%
 
 
 
-chart8 <- trip_times_drivethru_and_mode_mdt %>%
+trips_in_motion_p7 <-
+  trip_times_drivethru_and_mode_mdt %>%
   filter(mode_c != "missing") %>%
   ggplot(aes(x = time_band,y = rolling_count)) +
   geom_area(aes(fill = reorder(mode_c,desc(mode_c)))) +
@@ -289,7 +299,7 @@ chart8 <- trip_times_drivethru_and_mode_mdt %>%
              panel.grid.major.x = element_line(color = "light gray"),
              legend.max.columns = 3)
 
-finalize_plot(chart8,
+finalize_plot(trips_in_motion_p7,
               "Drive-thru / take-out trips in motion by time of day on weekdays.",
               "Note: Trips in motion are 55-minute rolling averages.
               <br><br>
