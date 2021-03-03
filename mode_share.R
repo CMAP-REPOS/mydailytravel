77ab946f0697a77c2318e125a7394f15c5d98dfe
Daniel Comeaux <58892984+dlcomeaux@users.noreply.github.com>
2021-03-03 17:20:01 -0600

    New mode share charts by demog

--- mode_share.R
+++ mode_share.R
@@ -10,7 +10,7 @@ library(ggplot2)
 library(tidyverse)
 library(cmapplot)
 
-source("pct_calculator.R")
+source("helper_fns.R")
 
 #################################################
 #                                               #
@@ -28,7 +28,7 @@ mdt_base_3 <-
   # school enrollment
   filter(age >= 16 |                   # 108293
            aage %in% c(4,5,6,7) |
-           schol %in% c(5,6,7,8)) %>%
+           age < 0 & schol %in% c(5,6,7,8)) %>%
   # Exclude "beginning" trips
   filter(mode_c != "beginning") %>%    # 84960
   # Exclude trips with zero distance
@@ -42,24 +42,24 @@ mdt_base_3 <-
     TRUE ~ mode_c)) %>%
   mutate(mode_c = factor(mode_c,levels = mode_c_levels))
 
-
-tt_base_3 <-
-  tt %>%                               # 140751 records
-  # Keep only travelers >= 16 years old, either through age, age bucket, or
-  # school enrollment
-  filter(AGE >= 16 |                   # 118886
-           SCHOL %in% c(5,6,7,8) |
-           AGEB == 2) %>%
-  # Keep only trips with nonzero distance
-  filter(DIST > 0) %>%                 # 89784
-  # Exclude missing modes
-  filter(mode_c != "missing") %>%      # 89784
-  # Put school bus back into "other" category
-  mutate(mode_c = as.character(mode_c)) %>%
-  mutate(mode_c = case_when(
-    mode_c == "schoolbus" ~ "other",
-    TRUE ~ mode_c)) %>%
-  mutate(mode_c = factor(mode_c,levels = mode_c_levels))
+#
+# tt_base_3 <-
+#   tt %>%                               # 140751 records
+#   # Keep only travelers >= 16 years old, either through age, age bucket, or
+#   # school enrollment
+#   filter(AGE >= 16 |                   # 118886
+#            SCHOL %in% c(5,6,7,8) |
+#            AGEB == 2) %>%
+#   # Keep only trips with nonzero distance
+#   filter(DIST > 0) %>%                 # 89784
+#   # Exclude missing modes
+#   filter(mode_c != "missing") %>%      # 89784
+#   # Put school bus back into "other" category
+#   mutate(mode_c = as.character(mode_c)) %>%
+#   mutate(mode_c = case_when(
+#     mode_c == "schoolbus" ~ "other",
+#     TRUE ~ mode_c)) %>%
+#   mutate(mode_c = factor(mode_c,levels = mode_c_levels))
 
 #################################################
 #                                               #
@@ -96,6 +96,56 @@ mdt_mode_counties <-
                               "93" = "Kendall",
                               "111" = "McHenry",
                               "197" = "Will"))
+
+mdt_mode_income <-
+  pct_calculator(mdt_base_3 %>% filter(hhinc > 0) %>%
+                   rbind(mdt_base_3 %>% filter(hhinc > 0) %>%
+                           mutate(hhinc = 99)),
+                 breakdown_by = "mode_c",
+                 second_breakdown = "hhinc",
+                 weight = "wtperfin") %>%
+  mutate(hhinc = recode_factor(factor(hhinc),
+                             "10" = "$150,000 or more",
+                             "9" = "$100,000 to $149,999",
+                             "8" = "$75,000 to $99,999",
+                             "7" = "$60,000 to $74,999",
+                             "6" = "$50,000 to $59,999",
+                             "5" = "$35,000 to $49,999",
+                             "4" = "$30,000 to $34,999",
+                             "3" = "$25,000 to $29,999",
+                             "2" = "$15,000 to $24,999",
+                             "1" = "Less than $15,000",
+                             "99" = "CMAP region average"))
+
+mdt_mode_race <-
+  pct_calculator(mdt_base_3 %>% filter(race_eth != "missing") %>%
+                   rbind(mdt_base_3 %>% filter(race_eth != "missing") %>%
+                           mutate(race_eth = "CMAP region average")),
+                 breakdown_by = "mode_c",
+                 second_breakdown = "race_eth",
+                 weight = "wtperfin") %>%
+  mutate(race_eth = recode_factor(factor(race_eth),
+                                  "other" = "Other",
+                                  "black" = "Black or African-American",
+                                  "asian" = "Asian",
+                                  "hispanic" = "Hispanic or Latino",
+                                  "white" = "White (non-Hispanic)"))
+
+
+# Age bins
+breaks <- c(-1,29, 49, 69, 150)
+age_labels <- c("16 to 29", "30 to 49",  "50 to 69", "70 and above")
+
+mdt_mode_age <-
+  pct_calculator(mdt_base_3 %>% filter(age > 0) %>%
+                   mutate(age_bin = cut(age, breaks = breaks,
+                                        labels = age_labels)) %>%
+                   rbind(mdt_base_3 %>% filter(age > 0) %>% mutate(age_bin = "CMAP region average")),
+                 breakdown_by = "mode_c",
+                 second_breakdown = "age_bin",
+                 weight = "wtperfin") %>%
+  mutate(age_bin = fct_relevel(age_bin,"CMAP region average")) %>%
+  mutate(age_bin = fct_rev(factor(age_bin)))
 
 ################################################################################
 # Chart of mode share by home county
@@ -143,6 +193,143 @@ finalize_plot(mode_share_p1,
               mode = "png",
               overwrite = T)
 
+################################################################################
+# Chart of mode share by income
+################################################################################
+
+mode_share_p2_labels <-
+  mdt_mode_income %>%
+  filter(mode_c %in% c("walk","transit","bike","other")) %>%
+  group_by(hhinc) %>%
+  summarize(label = sum(pct))
+
+mode_share_p2 <-
+  mdt_mode_income %>%
+  left_join(mode_share_p2_labels, by = "hhinc") %>%
+  mutate(mode_c = factor(mode_c,levels = c("driver","passenger","walk",
+                                           "transit","bike","other")),
+         pct = ifelse(mode_c %in% c("driver","passenger"),-1 *pct,pct)
+  ) %>%
+
+  ggplot(aes(x = pct, y = hhinc)) +
+  geom_col(aes(fill = mode_c),position = position_stack(reverse = T)) +
+  geom_label(data = mode_share_p2_labels,
+             aes(label = scales::label_percent(accuracy = 0.1)(label),
+                 x = label, y = hhinc),
+             label.size = 0,
+             hjust = 0,
+             fill = "white") +
+
+  theme_cmap(gridlines = "v", vline = 0, legend.max.columns = 10) +
+  cmap_fill_discrete(palette = "mobility") +
+  scale_x_continuous(n.breaks = 6, labels = scales::label_percent(),
+                     limits = c(-1,.55))
+
+finalize_plot(mode_share_p2,
+              title = "Mode share for trips in the CMAP region by household income,
+              highlighting non-car mode share.",
+              caption = "Note: Includes all trips in the CMAP region made by
+              travelers from ages 16 and older.
+              <br><br>
+              Source: Chicago Metropolitan Agency for Planning analysis of My
+              Daily Travel and Travel Tracker data.",
+              height = 5.5,
+              width = 11.3,
+              filename = "mode_share_p2",
+              mode = "png",
+              overwrite = T)
+
+################################################################################
+# Chart of mode share by race
+################################################################################
+
+mode_share_p3_labels <-
+  mdt_mode_race %>%
+  filter(mode_c %in% c("walk","transit","bike","other")) %>%
+  group_by(race_eth) %>%
+  summarize(label = sum(pct))
+
+mode_share_p3 <-
+  mdt_mode_race %>%
+  left_join(mode_share_p3_labels, by = "race_eth") %>%
+  mutate(mode_c = factor(mode_c,levels = c("driver","passenger","walk",
+                                           "transit","bike","other")),
+         pct = ifelse(mode_c %in% c("driver","passenger"),-1 *pct,pct)
+  ) %>%
+
+  ggplot(aes(x = pct, y = reorder(str_wrap(race_eth,18),label))) +
+  geom_col(aes(fill = mode_c),position = position_stack(reverse = T)) +
+  geom_label(data = mode_share_p3_labels,
+             aes(label = scales::label_percent(accuracy = 0.1)(label),
+                 x = label, y = str_wrap(race_eth,18)),
+             label.size = 0,
+             hjust = 0,
+             fill = "white") +
+
+  theme_cmap(gridlines = "v", vline = 0, legend.max.columns = 10) +
+  cmap_fill_discrete(palette = "mobility") +
+  scale_x_continuous(n.breaks = 6, labels = scales::label_percent(),
+                     limits = c(-1,.55))
+
+finalize_plot(mode_share_p3,
+              title = "Mode share for trips in the CMAP region by race and ethnicity,
+              highlighting non-car mode share.",
+              caption = "Note: Includes all trips in the CMAP region made by
+              travelers from ages 16 and older.
+              <br><br>
+              Source: Chicago Metropolitan Agency for Planning analysis of My
+              Daily Travel and Travel Tracker data.",
+              height = 5.5,
+              width = 11.3,
+              filename = "mode_share_p3",
+              mode = "png",
+              overwrite = T)
+
+################################################################################
+# Chart of mode share by age
+################################################################################
+
+mode_share_p4_labels <-
+  mdt_mode_age %>%
+  filter(mode_c %in% c("walk","transit","bike","other")) %>%
+  group_by(age_bin) %>%
+  summarize(label = sum(pct))
+
+mode_share_p4 <-
+  mdt_mode_age %>%
+  left_join(mode_share_p4_labels, by = "age_bin") %>%
+  mutate(mode_c = factor(mode_c,levels = c("driver","passenger","walk",
+                                           "transit","bike","other")),
+         pct = ifelse(mode_c %in% c("driver","passenger"),-1 *pct,pct)
+  ) %>%
+
+  ggplot(aes(x = pct, y = age_bin)) +
+  geom_col(aes(fill = mode_c),position = position_stack(reverse = T)) +
+  geom_label(data = mode_share_p4_labels,
+             aes(label = scales::label_percent(accuracy = 0.1)(label),
+                 x = label, y = age_bin),
+             label.size = 0,
+             hjust = 0,
+             fill = "white") +
+
+  theme_cmap(gridlines = "v", vline = 0, legend.max.columns = 10) +
+  cmap_fill_discrete(palette = "mobility") +
+  scale_x_continuous(n.breaks = 6, labels = scales::label_percent(),
+                     limits = c(-1,.55))
+
+finalize_plot(mode_share_p4,
+              title = "Mode share for trips in the CMAP region by age,
+              highlighting non-car mode share.",
+              caption = "Note: Includes all trips in the CMAP region made by
+              travelers from ages 16 and older.
+              <br><br>
+              Source: Chicago Metropolitan Agency for Planning analysis of My
+              Daily Travel and Travel Tracker data.",
+              height = 5.5,
+              width = 11.3,
+              filename = "mode_share_p4",
+              mode = "png",
+              overwrite = T)
 ################################################################################
 # Archive - Chart of overall mode share
 ################################################################################
