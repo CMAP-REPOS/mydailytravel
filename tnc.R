2f1b1fe4cfd3f69ab7068bf52678467c0e383929
Daniel Comeaux <58892984+dlcomeaux@users.noreply.github.com>
2021-03-15 17:41:22 -0500

    Clean and review
    Update to account for variables pulled into main data table

--- tnc.R
+++ tnc.R
@@ -1,5 +1,3 @@
-#### TO BE REVIEWED
-
 # This script analyzes TNC usage in MDT, using a combination of travel diary and
 # survey entries
 
@@ -23,65 +21,35 @@ setwd("~/GitHub/mydailytravel")
 source("helper_fns.R")
 source("data_cleaning.R")
 
-
-# Create base data set with TNC variables (not included in the base data pull)
-tnc_base <- read_csv("C:/Users/dcomeaux/Chicago Metropolitan Agency for Planning/Transportation Focus Area - Documents/My Daily Travel 2020/2018 survey/Data/person.csv") %>%
-  select(sampno,
-         perno,
-         age,
-         sex,
-         hisp,
-         race,
-         smrtphn,
-         wkstat,
-         tnc_use,
-         tnc_typ,
-         tnc_cost,
-         tnc_purp,
-         disab,
-         pertrips,
-         wtperfin)
-
 # Calculate the number of trips taken by travelers on active modes like transit,
 # biking, and walking, as well as TNC trips
 active_travel <-
   mdt %>%
+  # Exclude beginning trips
+  filter(mode_c != "beginning") %>%  # 97374
+  # Exclude trips with no travel distance.
+  filter(distance_pg > 0) %>%        # 97316
+  # Group data by the traveler level
   group_by(sampno,perno) %>%
+  # Create flags for transit, biking, walking, and tnc trips
   summarize(takes_transit = sum(mode_c == "transit",na.rm = TRUE),
             takes_bike = sum(mode_c == "bike",na.rm = TRUE),
             takes_walk = sum(mode_c == "walk",na.rm = TRUE),
-            takes_tnc = sum((mode == "rideshare") | (mode == "shared rideshare"),na.rm = TRUE)) %>%
+            takes_tnc = sum((mode == "rideshare") | 
+                              (mode == "shared rideshare"),na.rm = TRUE)) %>%
+  # Create a flag that is 1 if any of transit, biking, or walking is nonzero,
+  # and zero otherwise
   mutate(takes_active = min(sum(takes_transit,takes_bike,takes_walk),1))
-
-
-
-# Call "recoding.R" to get helper vectors and lists for recoding
-source("recoding.R")
 
 # Create working dataset based on variables pulled above and person/hh
 # characteristics from other tables
 tnc <-
-  tnc_base %>%                          # 30,683 records
-  # Add home county
-  inner_join(home, by = "sampno") %>%   # 30,683 records
-  # Add household statistics
-  inner_join(hh, by = "sampno") %>%     # 30,683 records
-  # Recode race and ethnicity (uses same logic as recoding.R)
-  mutate(race_eth =
-           recode(race,
-                  "1" = "white","2" = "black",
-                  "3" = "asian","4" = "other",
-                  "5" = "other","6" = "other",
-                  "97" = "other","-8" = "missing",
-                  "-7" = "missing")) %>%
-  # Recode Hispanic status (uses same logic as recoding.R)
-  mutate(race_eth = case_when(
-    hisp == 1 ~ "hispanic",
-    TRUE ~ race_eth))  %>%
-  # Bucket household income (uses same logic as recoding.R)
-  mutate(income = factor(hhinc),
-         income = recode_factor(income,!!!recode_income_detailed_mdt)) %>%
-  mutate(income_c = fct_collapse(income,!!!recode_income_buckets_mdt)) %>%
+  mdt_all_respondents %>%
+  # Keep only travelers 18+ (they are the ones that answered TNC survey
+  # questions)
+  filter(age >= 18 |
+           (age < 0 & aage %in% c(5,6,7)) |
+           (age < 0 & age18 == 1)) %>% 
   # Add information on active travel from above
   left_join(active_travel, by = c("sampno","perno")) %>%
   # Add 0s for people with no trips (who otherwise are NAs)
@@ -114,21 +82,13 @@ tnc <-
            TRUE ~ 0
          ))
 
-# Remove recode helpers
-rm(recode_income_buckets_mdt,recode_income_buckets_tt,
-   recode_mode_buckets_mdt,recode_mode_buckets_tt,
-   recode_tpurp_buckets_mdt,recode_tpurp_buckets_tt,
-   recode_income_detailed_mdt,recode_income_detailed_tt,
-   recode_mode_detailed_mdt,recode_mode_detailed_tt,
-   recode_tpurp_detailed_mdt,recode_tpurp_detailed_tt)
-
 # Age bins
-breaks <- c(-1, 10, 17, 29, 39, 49, 59, 69, 79, 89)
+breaks <- c(-1, 9, 17, 29, 39, 49, 59, 69, 79, 89)
 age_labels <- c("5 to 9", "10 to 17", "18 to 29", "30 to 39", "40 to 49",
                 "50 to 59", "60 to 69", "70 to 79", "80 to 89")
 
-# Reshape data
-tnc <- tnc %>%
+# Reshape data to add flags for each county as individual columns
+tnc_wide <- tnc %>%
   pivot_wider(
     names_from = county,
     id_cols = c(sampno:takes_active,white:high_income),
@@ -150,31 +110,47 @@ tnc <- tnc %>%
 
 # Run linear regression
 tnc_use_lm <-
-  tnc %>%
-  filter(!(tnc_use %in% c(-9,-8,-7,-1))) %>%
-  filter(income_c != "missing") %>%
-  filter(!(hhveh %in% c(-9,-8,-7)),
-         pertrips != -1,
-         race_eth != "missing") %>%
-  #filter(county_fips == 31) %>%
-  #group_by(county_fips) %>%
+  # Get data
+  tnc_wide %>%
+  # Filter out respondents who don't have an answer for...
+    # TNC use
+    filter(!(tnc_use %in% c(-9,-8,-7,-1))) %>%
+    # Household incomes
+    filter(income_c != "missing") %>%
+    # Household vehicles
+    filter(!(hhveh %in% c(-9,-8,-7))) %>% 
+    # Trips
+    filter(pertrips != -1) %>% 
+    # Race and ethnicity
+    filter(race_eth != "missing") %>% 
+    # Smartphone
+    filter(!(smrtphn %in% c(-9,-8,-7,-1))) %>%
+  # Recode smartphone to be a dummy with 1 as "yes" and 0 as "no" (previously
+  # "no" was coded as 2)
+  mutate(smrtphn = recode(smrtphn,
+                          "1" = "1",
+                          "2" = "0")) %>% 
+  
+  # Execute linear regression
   lm(tnc_use ~
-       takes_transit +
-       takes_bike +
-       takes_walk +
-       county_31 +
-       #county_43 + county_89 + county_93 +
-       #county_97 + county_111 + county_197 +
-       high_income +
-       white +
-       black +
-       hispa +
-       asian +
-       # other +
-       hhveh +
-       pertrips,
+       takes_transit + # How many times did they take transit on their travel day?
+       takes_bike + # How many times did they bike on their travel day?
+       takes_walk + # How many times did they walk on their travel day?
+       county_31 + # Is their home in Cook County?
+       high_income + # Is their household income above $60,000/year?
+       hhveh + # How many household vehicles do they have?
+       smrtphn + # Do they have a smartphone?
+       pertrips + # How many trips did they take on their travel day?
+       # Are they: 
+       white + # White (non-Hispanic)?
+       black + # Black?
+       hispa + # Hispanic (of any race)?
+       asian, # Asian?
       .,
-      weights = wtperfin)
+      # We use the weights to better represent the regional trends (vs. raw
+      # responses)
+      weights = wtperfin
+     )
 
 summary(tnc_use_lm)
 
