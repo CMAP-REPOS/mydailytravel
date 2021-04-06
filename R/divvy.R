26735c86b5e575e6815690525f8fcdeccb71ba76
Daniel Comeaux <58892984+dlcomeaux@users.noreply.github.com>
2021-04-06 13:06:42 -0500

    Shift to MDT survey timing

--- R/divvy.R
+++ R/divvy.R
@@ -22,29 +22,31 @@ library(cmapplot)
 setwd("~/GitHub/mydailytravel")
 source("R/helper_fns.R")
 
-# Download 2019 data (split into four quarters)
+# Download 2018-2019 data (split into four quarters across the survey collection period)
 divvy_zip <- tempfile()
+
+download.file("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q3.zip",divvy_zip)
+divvy_18q3 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2018_Q3.csv"))
+
+download.file("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",divvy_zip)
+divvy_18q4 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2018_Q4.csv"))
+
 download.file("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",divvy_zip)
-divvy_q1 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2019_Q1"))
+divvy_19q1 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2019_Q1"))
 
 download.file("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",divvy_zip)
-divvy_q2 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2019_Q2"))
+divvy_19q2 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2019_Q2"))
 
-download.file("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",divvy_zip)
-divvy_q3 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2019_Q3.csv"))
-
-download.file("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",divvy_zip)
-divvy_q4 <-read.csv(unzip(divvy_zip,files = "Divvy_Trips_2019_Q4.csv"))
 
 file.remove(divvy_zip,"Divvy_Trips_2019_Q1","Divvy_Trips_2019_Q2",
-            "Divvy_Trips_2019_Q3.csv","Divvy_Trips_2019_Q4.csv")
+            "Divvy_Trips_2018_Q3.csv","Divvy_Trips_2018_Q4.csv")
 rm(divvy_zip)
 
 # Fix names of q2, which are broken for unknown reasons on import
-names(divvy_q2) <- names(divvy_q1)
+names(divvy_19q2) <- names(divvy_19q1)
 
 # Combine all 2019 data
-divvy <- rbind(divvy_q1,divvy_q2,divvy_q3,divvy_q4)
+divvy <- rbind(divvy_18q3,divvy_18q4,divvy_19q1,divvy_19q2)
 
 
 #################################################
@@ -59,51 +61,55 @@ divvy <- rbind(divvy_q1,divvy_q2,divvy_q3,divvy_q4)
 threshold <- as.numeric(ymd_hms("2020-01-01 03:00:00", tz = "America/Chicago"))
 # The number of seconds in a day (used to add days)
 day_value <- 60*60*24
-# Holidays: New Year's Day, MLK Day, President's Day, Memorial Day, July 4th,
-# Labor Day, Columbus Day, Veterans Day, Thanksgiving, the Day after
-# Thanksgiving, and Christmas Day.
-nyd <-      interval(ymd_hms("2019-01-01 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-01-02 02:59:59",tz = "America/Chicago"))
+
+
+# We want to keep all weekdays from September 4th, 2018 to May 9th, 2019. This
+# represents the time period during which the main phase of the travel survey
+# was being actively completed. However, several weeks were excluded that need
+# to be excluded here as well: the week of Thanksgiving 2018 (November 19th to
+# November 23rd); the period between December 24th, 2018 and January 4th, 2019;
+# and the week of Spring Break (April 15th to 19th, 2019).
+xgiving <-  interval(ymd_hms("2018-11-19 03:00:00",tz = "America/Chicago"),
+                     ymd_hms("2018-11-23 02:59:59",tz = "America/Chicago"))
+xmas <-     interval(ymd_hms("2018-12-24 03:00:00",tz = "America/Chicago"),
+                     ymd_hms("2019-01-04 02:59:59",tz = "America/Chicago"))
+springb <-  interval(ymd_hms("2019-04-15 03:00:00",tz = "America/Chicago"),
+                     ymd_hms("2019-04-19 02:59:59",tz = "America/Chicago"))
+
+# We also exclude federal public holidays that fall within this time range that
+# were not excluded through the date ranges above: Columbus Day, Veterans Day,
+# MLK Day, and President's Day,.
+columbus <- interval(ymd_hms("2018-10-08 03:00:00",tz = "America/Chicago"),
+                     ymd_hms("2018-10-09 02:59:59",tz = "America/Chicago"))
+vets <-     interval(ymd_hms("2018-11-12 03:00:00",tz = "America/Chicago"),
+                     ymd_hms("2018-11-13 02:59:59",tz = "America/Chicago"))
 mlk <-      interval(ymd_hms("2019-01-21 03:00:00",tz = "America/Chicago"),
                      ymd_hms("2019-01-22 02:59:59",tz = "America/Chicago"))
 pres <-     interval(ymd_hms("2019-02-18 03:00:00",tz = "America/Chicago"),
                      ymd_hms("2019-02-19 02:59:59",tz = "America/Chicago"))
-memorial <- interval(ymd_hms("2019-05-27 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-05-28 02:59:59",tz = "America/Chicago"))
-july4 <-    interval(ymd_hms("2019-07-04 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-07-05 02:59:59",tz = "America/Chicago"))
-labor <-    interval(ymd_hms("2019-09-02 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-09-03 02:59:59",tz = "America/Chicago"))
-columbus <- interval(ymd_hms("2019-10-14 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-10-15 02:59:59",tz = "America/Chicago"))
-vets <-     interval(ymd_hms("2019-11-11 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-11-12 02:59:59",tz = "America/Chicago"))
-xgiving <-  interval(ymd_hms("2019-11-28 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-11-29 02:59:59",tz = "America/Chicago"))
-blackfri <- interval(ymd_hms("2019-11-29 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-11-30 02:59:59",tz = "America/Chicago"))
-xmas <-     interval(ymd_hms("2019-12-25 03:00:00",tz = "America/Chicago"),
-                     ymd_hms("2019-12-26 02:59:59",tz = "America/Chicago"))
-# Join all holidays into one list
-holidays <- c(nyd,mlk,pres,memorial,july4,labor,
-              columbus,vets,xgiving,blackfri,xmas)
 
-# Identify number of non-holiday weekdays in the sample
+# Join all holidays into one list
+holidays <- c(mlk,pres,columbus,vets)
+
+# Identify number of non-excluded weekdays in the sample
 number_of_weekdays <-
-  sum(!weekdays(seq(ymd("2019-01-01"),
-                    ymd("2019-12-31"),
+  sum(!weekdays(seq(ymd("2018-09-04"),
+                    ymd("2019-05-09"),
                     by = "days")) %in% c("Saturday", "Sunday")) -
-  length(holidays)
+  length(holidays) -
+  5 - # for Thanksgiving week
+  10 - # for Christmas/New Year's weeks
+  5   # for Spring Break week
 
 # Clean and filter data for application of the TIM calculator function
 divvy_wip <-
-  divvy %>% # 3818004 records
+  divvy %>% # 3629488 records
   # Convert to datetime object
   mutate(start_time = ymd_hms(start_time, tz = "America/Chicago"),
          end_time = ymd_hms(end_time, tz = "America/Chicago")) %>%
   mutate(trip_time = end_time - start_time) %>%
   # Exclude trips > 3 hours as outliers
-  filter(trip_time <= 60 * 60 * 3) %>% # 3817608 records
+  filter(trip_time <= 60 * 60 * 3) %>% # 3629175 records
   
   # Identify the day of the week of the trip using the `wday` function from the
   # lubridate package. Note that we subtract 3 hours from the day to use this
@@ -112,20 +118,16 @@ divvy_wip <-
   # Friday.
   mutate(wday = wday(start_time - 3 * 60 * 60)) %>%
   # Keep out trips that are either Saturday (7) or Sunday (1)
-  filter(!(wday %in% c(1,7))) %>% # 2904294 records
+  filter(!(wday %in% c(1,7))) %>% # 2771405 records
   # Remove holidays (individually - note that %within% does not currently work
   # on a list of time intervals)
-  filter(!(start_time %within% nyd | 
-           start_time %within% mlk |
+  filter(!(start_time %within% mlk |
            start_time %within% pres |
-           start_time %within% memorial |
-           start_time %within% july4 |
-           start_time %within% labor |
            start_time %within% columbus |
            start_time %within% vets |
            start_time %within% xgiving |
-           start_time %within% blackfri |
-           start_time %within% xmas)) %>% # 2833191 records
+           start_time %within% xmas |
+           start_time %within% springb)) %>% # 2660889 records
   
   # Make every trip on the same day (for analysis and graphing)
   mutate(trip_start = force_tz(ymd_hms(paste0("2020-01-01 ",
@@ -177,26 +179,52 @@ divvy_p1 <-
   # Add CMAP style
   cmap_fill_discrete(palette = "legislation") +
   theme_cmap(gridlines = "hv",
-             panel.grid.major.x = element_line(color = "light gray"))
+             panel.grid.major.x = element_line(color = "light gray"),
+             xlab = "Divvy trips in motion on weekdays by customer type")
 
 # Export plot
 finalize_plot(divvy_p1,
-              title = "Divvy trips in motion on an average weekday, by
-              customer type.",
+              title = "Divvy ridership is significantly higher in the afternoon, 
+              especially for non-subscribers.",
               caption = "Note: Trips in motion are 25-minute rolling averages.
-              \"One-time user\" refers to Divvy customers that purchased a day
-              pass or one ride. Trips that were in motion as of 2:55 A.M. and 
-              ended after 3:00 A.M. are captured on the right-hand-side of the 
-              graph, and are not included in the totals of trips in motion as 
-              of 3:00 A.M. on the left-hand-side of the graph.
+              \"One-time user\" refers to Divvy customers that purchased a 
+              single ride or a day pass. Trips that were in motion as of 2:55 
+              A.M. and ended after 3:00 A.M. are captured on the right-hand-side 
+              of the graph, and are not included in the totals of trips in  
+              motion as of 3:00 A.M. on the left-hand-side of the graph.
               <br><br>
               Source: Chicago Metropolitan Agency for Planning analysis of Divvy
-              ridership data for the 250 non-holiday weekdays between January 1 
-              and December 31, 2019.",
+              ridership data for weekdays during the My Daily Travel survey 
+              collection period, between September 4, 2018 and May 9, 2019 
+              (excluding all public holidays and the weeks of November 19th, 
+              December 24th, December 31st, April 15th).",
               filename = "divvy_p1",
               # sidebar_width = 0,
               # caption_align = 1,
-              mode = "png",
+              # mode = "png",
               # height = 6.3,
               # width = 11.3,
               overwrite = T)
+
+####### ARCHIVE OF 2019 HOLIDAYS (FOR FUTURE ANALYSIS
+
+# mlk <-      interval(ymd_hms("2019-01-21 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-01-22 02:59:59",tz = "America/Chicago"))
+# pres <-     interval(ymd_hms("2019-02-18 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-02-19 02:59:59",tz = "America/Chicago"))
+# memorial <- interval(ymd_hms("2019-05-27 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-05-28 02:59:59",tz = "America/Chicago"))
+# july4 <-    interval(ymd_hms("2019-07-04 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-07-05 02:59:59",tz = "America/Chicago"))
+# labor <-    interval(ymd_hms("2019-09-02 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-09-03 02:59:59",tz = "America/Chicago"))
+# columbus <- interval(ymd_hms("2019-10-14 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-10-15 02:59:59",tz = "America/Chicago"))
+# vets <-     interval(ymd_hms("2019-11-11 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-11-12 02:59:59",tz = "America/Chicago"))
+# xgiving <-  interval(ymd_hms("2019-11-28 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-11-29 02:59:59",tz = "America/Chicago"))
+# blackfri <- interval(ymd_hms("2019-11-29 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-11-30 02:59:59",tz = "America/Chicago"))
+# xmas <-     interval(ymd_hms("2019-12-25 03:00:00",tz = "America/Chicago"),
+#                      ymd_hms("2019-12-26 02:59:59",tz = "America/Chicago"))
