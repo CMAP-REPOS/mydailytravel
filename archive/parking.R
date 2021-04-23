dc8a65ff8c061ecbe2b2a9ff1ce65b984722742e
Daniel Comeaux <58892984+dlcomeaux@users.noreply.github.com>
2021-04-23 14:52:57 -0500

    Change parking analysis

--- archive/parking.R
+++ archive/parking.R
@@ -22,37 +22,46 @@ source("R/data_cleaning.R")
 
 
 # Join the first household responder for MDT with vehicle information
-household_vehs <-
+household_parking <-
   mdt_all_respondents %>% 
   select(sampno,perno,wtperfin,hhinc,hhveh,
          home_state,home_county,home_tract,home_county_chi,geog,
          income,income_c,race_eth) %>% 
   filter(perno == 1) %>% 
   select(-perno) %>%
-  inner_join(veh, by = "sampno") %>% 
-  mutate(fuel = recode_factor(factor(fuel,levels = c(1,2,3,4,5,97,-7,-8)),
-                              "1" = "Gas",
-                              "2" = "Diesel",
-                              "3" = "Hybrid",
-                              "4" = "EV",
-                              "5" = "Alternative",
-                              "97" = "Other",
-                              "-7" = "Missing",
-                              "-8" = "Missing"),
-         parkd = recode_factor(factor(parkd,levels = c(1,2,3,97,-7,-8)),
+  left_join(veh, by = "sampno") %>% 
+  replace_na(list(fuel = 0,parkd = 0)) %>% 
+  filter(parkd > -1) %>%
+  group_by(sampno,wtperfin,hhinc,hhveh,
+           home_state,home_county,home_tract,home_county_chi,geog,
+           income,income_c,race_eth) %>% 
+  summarize(park_simp = min(parkd)) %>% 
+  mutate(park_simp = recode_factor(factor(park_simp,levels = c(0,1,2,3,97)),
+                               "0" = "No vehicle",
                                "1" = "On street",
                                "2" = "Off street",
-                               "3" = "In my garage",
-                               "97" = "Other",
-                               "-7" = "Missing",
-                               "-8" = "Missing"))
+                               "3" = "Off street",
+                               "97" = "Other"))
 
 pct_calculator(
-  household_vehs %>% 
-    filter(parkd %in% c("On street","Off street", "In my garage"),
-           geog != "Other",
-           race_eth != "Missing"),
-  breakdown_by = "race_eth",
-  second_breakdown = "parkd",
+  household_parking %>% 
+    filter(geog != "Other",
+           income_c != "missing",
+           park_simp != "Other"),
+  breakdown_by = "income_c",
+  second_breakdown = "park_simp",
   third_breakdown = "geog",
-  weight = "wtperfin") %>% arrange(geog,parkd) %>% View()
+  weight = "wtperfin") %>% arrange(park_simp,income_c) %>% View()
+
+
+### RECODING FOR FUEL
+  # mutate(fuel = recode_factor(factor(fuel,levels = c(0,1,2,3,4,5,97,-7,-8)),
+  #                             "0" = "No vehicle",
+  #                             "1" = "Gas",
+  #                             "2" = "Diesel",
+  #                             "3" = "Hybrid",
+  #                             "4" = "EV",
+  #                             "5" = "Alternative",
+  #                             "97" = "Other",
+  #                             "-7" = "Missing",
+  #                             "-8" = "Missing"),
