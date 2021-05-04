e24950527c7fd075880d0a1e3d2e9e56896ffded
Daniel Comeaux <58892984+dlcomeaux@users.noreply.github.com>
2021-05-04 17:00:23 -0500

    Fix weight dropping and chart tweaks

--- R/racial_disparities.R
+++ R/racial_disparities.R
@@ -10,7 +10,6 @@
 library(ggplot2)
 library(tidyverse)
 library(cmapplot)
-library(MetricsWeighted)
 
 #################################################
 #                                               #
@@ -27,30 +26,38 @@ source("R/data_cleaning.R")
 all_trips_mdt <-
   mdt %>%                            # 125463 records
   filter(
-    # Keep only:                     # 108612
-    # Those 16 or older
+    # Keep only:                     # 125447
+    # Those 5 or older
     age >= 5 |
       # or those in an age category from 5 to 44
       (age < 0 & aage %in% c(2,3,4,5,6,7)) |
       # Or those enrolled in school grades we know are 5+
       (age < 0 & schol %in% c(4,5,6,7,8))) %>%
   # Exclude "beginning" trips
-  filter(mode_c != "beginning") %>%  # 85014
+  filter(mode_c != "beginning") %>%  # 97635
   # Keep only trips with nonzero distance
-  filter(distance_pg > 0) %>%        # 84961
+  filter(distance_pg > 0) %>%        # 97307
   # Exclude missing mode trips
-  filter(mode_c != "missing") %>%    # 84924
-  ungroup()
-  # # Code to exclude high and low weight households, by zone
-  # left_join(zones, by = "sampno") %>%
-  # arrange(wtperfin) %>%
+  filter(mode_c != "missing") %>%    # 97270
+  # # Code to exclude high and low weight households, by zone.
   # group_by(cluster) %>%
+  # arrange(cluster,wtperfin) %>%
+  # # Identify the ranked order of this record by weight
   # mutate(rank = row_number()) %>%
+  # # Find the total number of records in the cluster
   # mutate(max_rank = max(rank)) %>%
+  # # Divide the row's rank by the total
   # mutate(pct_rank = rank / max_rank) %>%
-  # filter(pct_rank >= 0.05,
-  #        pct_rank <= 0.95) %>%
-  # ungroup()
+  # ungroup() %>% 
+  # # Since there are ties in weights, find the highest and lowest percent for a
+  # # given weight
+  # group_by(cluster,wtperfin) %>% 
+  # mutate(max_wt_pct = max(pct_rank),
+  #        min_wt_pct = min(pct_rank)) %>% 
+  # filter(min_wt_pct >= 0.05,
+  #        max_wt_pct <= 0.95) %>%      # 3179 records
+  ungroup()
+
 
 #################################################
 #                                               #
@@ -65,7 +72,7 @@ all_trips_mdt <-
 
 # Filter data
 work_time_race_mdt <-
-  all_trips_mdt %>%                  # 84294 records
+  all_trips_mdt %>%                  # 97270 records
   filter(
     # Keep only trips of employed respondents (either those who report having 1+
     # jobs or those who report being employed)
@@ -74,15 +81,15 @@ work_time_race_mdt <-
   # trips)
   filter(tpurp == "Worked at fixed work location") %>%  # 14160
   # Keep only trips to identified work locations
-  filter(loctype == 2) %>%           # 12099
+  filter(loctype == 2) %>%           # 12111
   ungroup() %>% 
   # Only include trips that are more than 0 minutes and less than 2.5 hours
   filter(
     travtime_pg_calc < 150 &
-    travtime_pg_calc > 0) %>% # 12044 records
+    travtime_pg_calc > 0) %>% # 12066 records
   
   # Exclude households with missing race and ethnicity information
-  filter(race_eth != "missing") %>% # 12012 records
+  filter(race_eth != "missing") %>% # 12034 records
   
   # Calculate weighted median of trip times by race and ethnicity
   group_by(race_eth,tpurp) %>%
@@ -94,12 +101,12 @@ work_time_race_mdt <-
 
 # Filter data
 other_time_race_mdt <-
-  all_trips_mdt %>%                  # 84294 records
+  all_trips_mdt %>%                  # 97270 records
   filter(tpurp %in% c("Health care visit for self",
                       "Health care visit for someone else",
                       "Visited a person staying at the hospital",
                       "Shopped (routine like grocery, clothing)"
-                      )) %>%  # 9417
+                      )) %>%  # 8769
   # Recode healthcare
   mutate(tpurp = recode_factor(tpurp,
                                "Health care visit for someone else" = "Health care",
@@ -109,10 +116,10 @@ other_time_race_mdt <-
   # Only include trips that are more than 0 minutes and less than 2.5 hours
   filter(
     travtime_pg_calc < 150 &
-      travtime_pg_calc > 0) %>% # 12044 records
+      travtime_pg_calc > 0) %>% # 8732 records
   
   # Exclude households with missing race and ethnicity information
-  filter(race_eth != "missing") %>% # 12012 records
+  filter(race_eth != "missing") %>% # 8693 records
   
   # Calculate weighted mean of trip times by race and ethnicity
   group_by(race_eth,tpurp) %>%
@@ -133,14 +140,15 @@ racial_disparities_p1 <-
                            "asian" = "Asian","other" = "Other",
                            "hispanic" = "Hispanic")) %>%
   mutate(tpurp_disp = as.character(tpurp)) %>% 
-  mutate(tpurp_disp = recode(factor(tpurp_disp, 
-                                    levels = c("Worked at fixed work location",
-                                               "Health care",
-                                               "Shopped (routine like grocery, clothing)")),
-                             "Worked at fixed work location" = "Work (fixed location)",
-                             "Health care visit for self" = "Health care (personal)",
-                             "Shopped (routine like grocery, clothing)" = "Routine shopping (e.g., groceries or clothing)",
-                             "Went to the gym" = "Gym")) %>% 
+  mutate(tpurp_disp = 
+           recode(factor(tpurp_disp, 
+                         levels = c("Worked at fixed work location",
+                                    "Health care",
+                                    "Shopped (routine like grocery, clothing)")),
+                  "Worked at fixed work location" = "Work (fixed location)",
+                  "Health care visit for self" = "Health care (personal)",
+                  "Shopped (routine like grocery, clothing)" = 
+                    "Routine shopping (e.g., groceries or clothing)",)) %>% 
   
   # Create ggplot object
   ggplot(aes(x = reorder(race_eth,-travtime), y = travtime, fill = race_eth)) +
@@ -163,14 +171,16 @@ racial_disparities_p1 <-
 
 # Export finalized graphic
 finalize_plot(racial_disparities_p1,
-              "Black residents of the region have significantly longer trips to 
+              "Black residents of the region had significantly longer trips to 
               work, health care, and routine shopping than those of 
               other residents.",
               caption = 
-              paste0("Note: Excludes travelers younger than 5. Trips to 'Work (fixed 
-              location)' only include trips made to fixed workplace locations by 
-              employed respondents. In all categories, trips with no travel time 
-              and those lasting more than two and a half hours are excluded. 
+              paste0("Note: Includes trips by residents aged 5 and older of the 
+              CMAP seven county region (Cook, DuPage, Kane, Kendall, Lake, 
+              McHenry, and Will), as well as Grundy and DeKalb. Includes only 
+              trips that were within, to, and/or from one of those counties. 
+              In all categories, trips with no travel time and those lasting 
+              more than two and a half hours are excluded. 
               'Hispanic' includes individuals of any racial group that identify 
               as Hispanic. All other categories are non-Hispanic.
               <br><br>
